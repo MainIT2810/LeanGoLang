29fef640f483f2df098a1621d00ba64cbfdb3aae
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-04-14 06:35:16 +0700

    viết function tính bmi và tạo file README.md

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "bufio" //thư viện chuẩn của Google
+	// "os"
+	
+	)
 
 func main() {
-	fmt.Println("Hello, world.")
+	// fmt.Println("Hello, world.")
+
+	/*
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your city:")
+	city, _ := reader.ReadString('\n')
+	fmt.Println("You live in", city)
+	*/
+
+	height := 1.68
+	weight := 80.0
+	bmi := CalculateBMI(height, weight)
+	fmt.Println("Your BMI is", bmi)
+}
+
+func CalculateBMI(height, weight float64) float64 {
+	return weight / (height * height)
 }
